0e260a5cda65653d038c551e114a94de5ac2da9e
Josh Wolfe <thejoshwolfe@gmail.com>
2026-08-24 15:12:42 -0400

    version 3.0.1

--- worlds/factorio_space_age/data/ap_data.py
+++ worlds/factorio_space_age/data/ap_data.py
@@ -1,7 +1,7 @@
 # This file is somewhat arbitrary constants chosen for the randomizer.
 # This file is inputs to both import-ap-dump.py and also the apworld proper.
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 # progressive_technologies: only_related
 small_progressive_groups = {
